eea1c07c23ef732d79e6700a8e5e17d39070e237
Uros Lazic <uros.lazic@protonmail.com>
2024-03-21 19:34:28 +0100

    3: fix IC & impl SelectStatement

--- 2023-jun2/treci.java
+++ 2023-jun2/treci.java
@@ -13,6 +13,7 @@ public class SelectStatement extends Statement {
 public class Alternative extends Statement {
     public Expression expr;
     public Statement stmt;
+    public String endLabel = "KRAJ";
 
     public Alternative(Expression expr, Statement stmt) {
         this.expr = expr;
@@ -28,15 +29,30 @@ public class Alternative extends Statement {
  * ALTERNATIVE:
  * IMC<expr>
  * Load R1, RESULT<expr>
- * JumpIfZero R1, KRAJ
+ * JumpIfZero R1, NEXTALTERNATIVE
  * IMC<stmt>
+ * Jump KRAJ
+ * NEXTALTERNATIVE:
  *
  * */
 
 //3. Impl za Alternative
 public void translate(BufferedWriter out) throws IOException {
+    String nextAlternative = ASTNode.genLab();
     expr.translate(out);
     expr.genLoad("R1", out);
-    out.write("JumpIfZero R1, KRAJ");
+    out.write(String.format("JumpIfZero R1, %s\n", nextAlternative));
     stmt.translate(out);
+    out.write(String.format("Jump %s\n", endLabel));
+    out.write(String.format("%s:\n", nextAlternative));
+}
+
+//4. Impl za SelectStatement
+public void translate(BufferedWriter out) throws IOException {
+    String endLabel = ASTNode.genLab();
+    for(Alternative alternative : alternativeList) {
+        alternative.endLabel = endLabel;
+        alternative.translate(out);
+    }
+    out.write(String.format("%s:\n", endLabel));
 }
