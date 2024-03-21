9a7ef7e48076b1ff994d724bf59fb980b24240b0
Uros Lazic <uros.lazic@protonmail.com>
2024-03-21 20:10:55 +0100

    3: impl ConditionalExpression

--- 2023-jun/treci.java
+++ 2023-jun/treci.java
@@ -49,3 +49,31 @@ public class ConditionalExpression extends Expression {
  * Store R3, result
  * */
 
+//3. Impl ConditionalExpression
+public void translate(BufferedWriter out) throws IOException {
+  String firstLabel = ASTNode.genLab();
+  String secondLabel = ASTNode.genLab();
+  String thirdLabel = ASTNode.genLab();
+  String endLabel = ASTNode.genLab();
+
+  testExpr.translate(out);
+  out.write(String.format("Load_Mem R1, %s\n", textExpr.result));
+  out.write("Load_Const R2, 0\n");
+  out.write("Compare_Less R1, R2\n");
+  out.write(String.format("JumpIfNotZero %s\n", firstLabel));
+  out.write("Compare_Equal R1, R2\n");
+  out.write(String.format("JumpIfNotZero %s\n", secondLabel));
+  out.write(String.format("Jump %s\n", thirdLabel));
+  out.write(String.format("%s:\n", firstLabel));
+  firstExpr.translate(out);
+  out.write(String.format("Load_Mem R3, %s\n", firstExpr.result));
+  out.write(String.format("Jump %s\n", endLabel));
+  secondExpr.translate(out);
+  out.write(String.format("Load_Mem R3, %s\n", secondExpr.result));
+  out.write(String.format("Jump %s\n", endLabel));
+  thirdExpr.translate(out);
+  out.write(String.format("Load_Mem R3, %s\n", thirdExpr.result));
+  out.write(String.format("%s:\n", endLabel));
+  out.write(String.format("Store R3, %s\n", this.result));
+
+}
