c9a2a8f2e37ffc52f3cf6a12dcea844e2bd7ba67
Uros Lazic <uros.lazic@protonmail.com>
2024-03-21 20:25:01 +0100

    3: definicija medjukoda

--- 2023-april/treci.java
+++ 2023-april/treci.java
@@ -13,3 +13,24 @@ public class RepeatStatement extends Statement {
   @Override
   public void translate(BufferedWriter out) throws IOException { ... }
 }
+
+//2. Definicja medjukoda
+/*
+ * RepeatStatement:
+ * Load_Const R4, 0 ; brojac
+ * Load_Const R5, times ; limit
+ * startLabel:
+ * Compare_Equal R4, R5
+ * JumpIfNotZero endLabel
+ * IMC<condition>
+ * Load_Mem R1, RESULT<condition>
+ * Load_Const R2, 0
+ * Compare_Equal R1, R2
+ * JumpIfNotZero endLabel
+ * IMC<statement> * number of statements
+ * Load_Const R3, 1
+ * ADD R4, R3 
+ * Jump startLabel
+ * endLabel:
+ * 
+ */
