afc66272e7092b02d435ca2bde790924f3b80cff
Uros Lazic <uros.lazic@protonmail.com>
2024-03-21 21:20:37 +0100

    4: asm

--- 2023-jun2/cetvrti.asm
+++ 2023-jun2/cetvrti.asm
@@ -27,38 +27,46 @@
 ;   │                 │
 ;   └─────────────────┘
 ;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
+render_scene:
+  push ebp     ;
+  mov ebp, esp ;  init stack frame
+  sub esp, 0x2 ;
+
+  ; prepare stack for load_model call
+  ; return value
+  mov [ebp-0x2], 0
+  mov eax, 0
+  push eax
+  ; load m
+  mov eax, [ebp+0x4]
+  ; m->type
+  mov ebx, [eax+0x2]
+  push ebx
+  ; m->id
+  mov ebx, [eax]
+  push ebx 
+  call load_model
+  add esp, 0x4
+  pop eax ; eax = result of load_model
+  ; loaded = loaded + result
+  mov ebx, [ebp-0x2]
+  add ebx, eax
+  mov [ebp-0x2], ebx
+  ; if
+  mov eax, [ebp+0x4]
+  mov eax, [eax+0x4]
+  test eax, eax ; set zero flag to 1 if eax = 0
+  jz kraj
+  ; prepare stack for render_schene
+  push eax
+  call render_scene
+  ; loaded = loaded + render_scene(m->next)
+  mov eax, [ebp-0x2]
+  add eax, ecx
+  mov [ebp-0x2], eax
+  kraj:
+  mov ecx, [ebp-0x2]
+  ; return
+  mov esp, ebp
+  pop ebp
+  ret
